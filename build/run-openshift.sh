dee9d974c43f5342b28b14fbd2434aac2af596f3
Jim Ma <ema@redhat.com>
2019-09-25 15:31:08 +0800

    some debug change

--- build/run-openshift.sh
+++ build/run-openshift.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
-
+wget https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz
+tar xvzOf openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz > oc.bin
+sudo mv oc.bin /usr/local/bin/oc
+sudo chmod 755 /usr/local/bin/oc
 oc login -u system:admin
 oc adm policy add-cluster-role-to-user cluster-admin developer
 oc apply -f deploy/service_account.yaml
