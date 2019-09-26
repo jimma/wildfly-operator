e5539e4af7b34f0804432b093e64680fb7e92065
Jim Ma <ema@redhat.com>
2019-09-26 16:10:58 +0800

    Use oc cluster up to test

--- build/run-openshift.sh
+++ build/run-openshift.sh
@@ -3,11 +3,5 @@ wget https://github.com/openshift/origin/releases/download/v3.11.0/openshift-ori
 tar xvzOf openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz > oc.bin
 sudo mv oc.bin /usr/local/bin/oc
 sudo chmod 755 /usr/local/bin/oc
+sudo ./oc cluster up --skip-registry-check=true
 oc login -u system:admin
-oc adm policy add-cluster-role-to-user cluster-admin developer
-oc apply -f deploy/service_account.yaml
-oc apply -f deploy/role.yaml
-oc apply -f deploy/role_binding.yaml
-oc apply -f deploy/crds/wildfly_v1alpha1_wildflyserver_crd.yaml
-oc apply -f deploy/operator.yaml
-oc login -u developer
--- ci/start-okd-4.0.0.sh
+++ ci/start-okd-4.0.0.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/env bash
 
-docker run --name oc-cluster-up -d -v /etc/docker/certs.d/:/certs/ -v /tmp/:/tmp/ -v /var/run/docker.sock:/var/run/docker.sock gustavonalle/oc-cluster-up
+#docker run --name oc-cluster-up -d -v /etc/docker/certs.d/:/certs/ -v /tmp/:/tmp/ -v /var/run/docker.sock:/var/run/docker.sock gustavonalle/oc-cluster-up
 
-function get_master {
-   echo $(docker exec openshift-master oc describe node/openshift-master-node  | grep InternalIP | awk '{print $2}')
-}
-
+#function get_master {
+#   echo $(docker exec openshift-master oc describe node/openshift-master-node  | grep InternalIP | awk '{print $2}')
+#}
+wget https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz
+tar xvzOf openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz > oc.bin
+sudo mv oc.bin /usr/local/bin/oc
+sudo chmod 755 /usr/local/bin/oc
+sudo oc cluster up 
 SLEEP=3
 MAX_WAIT=100
 
 echo "Waiting for OKD to be ready..."
 
-while ! curl -k -s "https://$(get_master):8443/healthz/ready"
+while ! curl -k -s "https://localhost:8443/healthz/ready"
 do
     echo "Still waiting..."
     ((c++)) && ((c==$MAX_WAIT)) && exit 1
