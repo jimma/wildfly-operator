432fd3e3655cd5886fc65f9e5e35c84330e78520
Jim Ma <ema@redhat.com>
2019-09-27 18:59:26 +0800

    Improve the travis build

--- build/run-openshift.sh
+++ build/run-openshift.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 wget https://github.com/openshift/origin/releases/download/v${OPENSHIFT_VERSION}/${OPENSHIFT_BIN}.tar.gz
-sudo tar xvzf ${OPENSHIFT_BIN} ${OPENSHIFT_BIN}/oc > /usr/local/bin/oc
+tar xvzf ${OPENSHIFT_BIN} ${OPENSHIFT_BIN}/oc > oc
+sudo mv oc /usr/local/bin/oc
 sudo chmod 755 /usr/local/bin/oc
 sudo oc cluster up --skip-registry-check=true
 sudo chmod 755 openshift.local.clusterup/kube-apiserver/admin.kubeconfig
