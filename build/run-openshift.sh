dc42cc35b9bbfce119a7c35a8a2c07ba8d2181ef
Jim Ma <ema@redhat.com>
2019-09-27 18:46:46 +0800

    Fix travis

--- build/run-openshift.sh
+++ build/run-openshift.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-wget https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz
-tar xvzOf openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz > oc.bin
-sudo mv oc.bin /usr/local/bin/oc
+wget https://github.com/openshift/origin/releases/download/v${OPENSHIFT_VERSION}/${OPENSHIFT_BIN}.tar.gz
+sudo tar xvzf ${OPENSHIFT_BIN} ${OPENSHIFT_BIN}/oc > /usr/local/bin/oc
 sudo chmod 755 /usr/local/bin/oc
 sudo oc cluster up --skip-registry-check=true
 sudo chmod 755 openshift.local.clusterup/kube-apiserver/admin.kubeconfig
