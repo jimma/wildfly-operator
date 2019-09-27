735eb13b786356921b7cafe91cc36fa79e496831
Jim Ma <ema@redhat.com>
2019-09-27 19:09:19 +0800

    Fix openshift.sh

--- build/run-openshift.sh
+++ build/run-openshift.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 wget https://github.com/openshift/origin/releases/download/v${OPENSHIFT_VERSION}/${OPENSHIFT_BIN}.tar.gz
-tar xvzf ${OPENSHIFT_BIN} ${OPENSHIFT_BIN}/oc > oc
+tar xvzf ${OPENSHIFT_BIN}.tar.gz ${OPENSHIFT_BIN}/oc > oc
 sudo mv oc /usr/local/bin/oc
 sudo chmod 755 /usr/local/bin/oc
 sudo oc cluster up --skip-registry-check=true
